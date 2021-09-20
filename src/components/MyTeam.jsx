a117d408f66e5af040946cb5fabd86be8b842617
gwynlordofcinder <pabloandrestejedor@icloud.com>
2021-09-20 00:04:35 -0300

    ui changes

--- src/components/MyTeam.jsx
+++ src/components/MyTeam.jsx
@@ -9,55 +9,53 @@ export const MyTeam = ({ heroTeam, setHeroTeam }) => {
   let averageWeight = getAverageWeight(heroTeam);
 
   return (
-    <>
-      <h1 className="mt-5">My team</h1>
-      <div className="m-5 d-flex">
-        <div className="d-flex text-center flex-column align-items-center">
-          <div className="m-1 p-3 border rounded" style={{width: '14rem'}}>
-            <h4>Overall Stats:</h4>
-            <div className="d-flex flex-column">
-              {overallStats.map(stat => (
-                <h5
-                  className={`m-1 p-2 border rounded ${stat.color}`}
-                  key={stat.stat}
-                >
-                  {stat.stat}: {stat.value || '-'}
-                </h5>
-              ))}
-            </div>
-
+    <div className="m-5">
+      <h1>My team</h1>
+      <div className="d-flex text-center">
+        <div className="m-3 p-3 border rounded">
+          <h4>Overall Stats:</h4>
+          <div className="d-flex">
+            {overallStats.map(stat => (
+              <h5
+                className={`m-1 p-2 border rounded ${stat.color}`}
+                key={stat.stat}
+              >
+                {stat.stat}: {stat.value || '-'}
+              </h5>
+            ))}
           </div>
-            <div className="m-3 p-3 border rounded">
-              <h4>Average Appearance:</h4>
-              <div className="d-flex flex-column">
-                <h5 className="m-2 p-1 border rounded bg-secondary">
-                  Height: {averageHeight || '-'} cm
-                </h5>
-                <h5 className="m-2 p-1 border rounded bg-secondary">
-                  Weight: {averageWeight || '-'} kg
-                </h5>
-              </div>
-            </div>
         </div>
 
-        {!heroTeam.length ? (
-          <p className="text-danger">
-            Your team does not contain any hero. Please scroll down and use the
-            search feature to seek and get heroes for your team
-          </p>
-        ) : null}
-
-        <div className="d-flex flex-wrap">
-          {heroTeam.map(hero => (
-            <HeroCardTeam
-              hero={hero}
-              heroTeam={heroTeam}
-              setHeroTeam={setHeroTeam}
-              key={hero.id}
-            />
-          ))}
+        <div className="m-3 p-3 border rounded">
+          <h4>Average Appearance:</h4>
+          <div className="d-flex">
+            <h5 className="m-2 p-3 border rounded bg-secondary">
+              Height: {averageHeight || '-'} cm
+            </h5>
+            <h5 className="m-2 p-3 border rounded bg-secondary">
+              Weight: {averageWeight || '-'} kg
+            </h5>
+          </div>
         </div>
       </div>
-    </>
+
+      {!heroTeam.length ? (
+        <p className="text-danger">
+          Your team will be shown here. At this moment, your team does not contain any hero. Please use the search feature to
+          get a hero for your team
+        </p>
+      ) : null}
+
+      <div className="d-flex">
+        {heroTeam.map(hero => (
+          <HeroCardTeam
+            hero={hero}
+            heroTeam={heroTeam}
+            setHeroTeam={setHeroTeam}
+            key={hero.id}
+          />
+        ))}
+      </div>
+    </div>
   );
 };
